620b074841e6a9b39e60070a408147f0015d39cb
Alejandra <ale.er018@gmail.com>
2024-07-08 16:41:00 -0300

    otro problema

---
+++ problema2.py
@@ -0,0 +1,11 @@
+
+hermano1=13
+hermano2=10
+
+if hermano1 > hermano2:
+    print("el hno1 es mayor que el hno2")
+elif hermano2 > hermano1:
+    print("el hno2 es mayor que el hno1")
+else:
+    print("tienen la misma edad")
+    
